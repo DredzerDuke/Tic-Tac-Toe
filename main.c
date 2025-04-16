7153d36e97b3383fe235bbdb51f09ab40852b7f4
Dredzer Duke <dukedredzer@gmail.com>
2025-04-16 10:28:01 +0530

    introduced the first working model

--- main.c
+++ main.c
@@ -1,118 +1,160 @@
 #include <stdio.h>
-#include <time.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <ctype.h>
+#include <time.h>
 
 char board[3][3];
-int chances = 0;
+const char PLAYER = 'X';
+const char CPU = 'O';
+char WINNER = ' ';
+int spaceAvailable = 9;
 
-void clear_board();
-void print_board();
-void take_userinput();
-bool check_empty(int r, int c);
-bool check_chances();
-void take_cpuinput();
+void printBoard();
+void resetBoard();
+char *getBoardPosition(int);
+void throwError(char*);
+bool isValidEntry(int);
+void makeEntry(int, bool);
+void checkWinner();
+void theGame();
+void displayWinner();
+int getPlayerInput();
+int getCpuInput();
 
-int main()
-{
-    clear_board();
-    while (check_chances())
-    {
-        take_cpuinput();
-        print_board();
-        take_userinput();
-        print_board();
-    }
-    printf("You ran out of chances!...\n");
+int main() {
+    theGame();
 }
 
-void clear_board()
-{
-    for (int i = 0; i < 3; i++)
-    {
-        for (int j = 0; j < 3; j++)
-        {
-            board[i][j] = ' ';
-        }
-    }
-}
-
-void print_board()
-{
-    printf("\n %c | %c | %c \n", board[0][0], board[0][1], board[0][2]);
-    printf("--- --- ---\n");
+void printBoard() {
+    printf(" %c | %c | %c \n", board[0][0], board[0][1], board[0][2]);
+    printf("---|---|---\n");
     printf(" %c | %c | %c \n", board[1][0], board[1][1], board[1][2]);
-    printf("--- --- ---\n");
-    printf(" %c | %c | %c \n\n", board[2][0], board[2][1], board[2][2]);
+    printf("---|---|---\n");
+    printf(" %c | %c | %c \n", board[2][0], board[2][1], board[2][2]);
+    printf("\n\n");
 }
 
-void take_userinput()
-{
-    while (1) 
-    {
-        int row, column;
-        printf("Enter the row : ");
-        scanf("%d", &row);
-        if (row > 3 || row < 0) {
-            printf("only row 1 to 3 accepted!\n");
-            continue;
-        }
-        row -= 1;
-        printf("Enter the column : ");
-        scanf("%d", &column);
-        if (column > 3 || column < 0) {
-            printf("only column 1 to 3 accepted!\n");
-            continue;
-        }
-        column -= 1;
-
-        if (check_chances() && check_empty(row, column))
-        {
-            board[row][column] = 'X';
-            break;
-        } else if (!check_empty(row, column))
-            printf("Try again..! \nThe place is occupied.\n");
-        else if (!check_chances()) {
-            printf("You ran out of chances!\n");
-            break;
-        }
-    }
-}
-
-bool check_empty(int r, int c)
-{
-    if (board[r][c] == ' ')
-        return true;
-    else
-        return false;
-}
-
-bool check_chances()
-{
+void resetBoard() {
+    int k = 49;
     for (int i = 0; i < 3; i++) {
-        for (int j = 0; j < 3; j++){
-            if (board[i][j] = ' ')
-                return true;
-        }
+        for (int j = 0; j < 3; j++)
+            board[i][j] = k++;
     }
-    return false;
 }
 
-void take_cpuinput()
-{
-    srand(time(NULL));
-    while (1)
-    {
-        int row = rand() % 3;
-        int column = rand() % 3;
-        if (check_chances() && check_empty(row, column))
-        {
-            board[row][column] = 'O';
+char *getBoardPosition(int position) {
+    switch (position) {
+        case 1:
+            return &board[0][0];
             break;
-        }
-        else if (!check_chances()) {
-            printf("You ran out of chances!\n");
-            exit(1);
-        }
+        case 2:
+            return &board[0][1];
+            break;
+        case 3:
+            return &board[0][2];
+            break;
+        case 4:
+            return &board[1][0];
+            break;
+        case 5:
+            return &board[1][1];
+            break;
+        case 6:
+            return &board[1][2];
+            break;
+        case 7:
+            return &board[2][0];
+            break;
+        case 8:
+            return &board[2][1];
+            break;
+        case 9:
+            return &board[2][2];
+            break;
     }
+    return "";
 }
+
+void throwError(char *errDesc) {
+    fprintf(stderr, "%s\n", errDesc);
+    exit(EXIT_FAILURE);
+}
+
+bool isValidEntry(int position) {
+    if (position < 1 || position > 9)
+        return false;
+    if (!spaceAvailable)
+        return false;
+    char *ch = getBoardPosition(position);
+    if (!isdigit(*ch))
+        return false;
+    return true;
+}
+
+void makeEntry(int position, bool isPlayer) {
+    if (!isValidEntry(position))
+        throwError("not a valid entry!");
+    char *boardPosition = getBoardPosition(position);
+    if (isPlayer)
+        *boardPosition = PLAYER;
+    else
+        *boardPosition = CPU;
+    spaceAvailable--;
+    checkWinner();
+}
+
+void checkWinner() {
+    for (int i = 0; i < 3; i++) {
+        if (board[i][0] == board[i][1] && board[i][0] == board[i][2])
+            WINNER = board[i][0];
+    }
+    for (int i = 0; i < 3; i++) {
+        if (board[0][i] == board[1][i] && board[0][i] == board[2][i])
+            WINNER = board[0][i];
+    }
+    if (board[0][0] == board[1][1] && board[0][0] == board[2][2])
+        WINNER = board[0][0];
+    if (board[0][2] == board[1][1] && board[0][2] == board[2][0])
+        WINNER = board[0][2];
+}
+
+int getPlayerInput() {
+    int userInput;
+    printf("Enter your choice : ");
+    scanf("%d", &userInput);
+    getchar();
+    return userInput;
+}
+
+int getCpuInput() {
+    int cpuInput;
+    srand(time(NULL));
+    do {
+        cpuInput = rand() % 9 + 1;
+    } while (!isValidEntry(cpuInput));
+    return cpuInput;
+}
+
+void displayWinner() {
+    if (WINNER == PLAYER)
+        printf("Congrats! You Win....!\n");
+    else if (WINNER == CPU)
+        printf("CPU won... Try Again!\n");
+    else
+        printf("its a draw!\n");
+}
+
+void theGame() {
+    resetBoard();
+    printBoard();
+    while(WINNER == ' ' && spaceAvailable > 0) {
+        int playerInput = getPlayerInput();
+        makeEntry(playerInput, true);
+        int cpuInput = getCpuInput();
+        makeEntry(cpuInput, false);
+        printBoard();
+    }
+    displayWinner();
+}
+
